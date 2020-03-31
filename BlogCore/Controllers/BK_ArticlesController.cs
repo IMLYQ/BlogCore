94feadeda052d311f1849a6ffafb551a150ff44b
yongqing liang <1451468553@qq.com>
2020-03-31 11:46:10 +0800

    。。。

--- BlogCore/Controllers/BK_ArticlesController.cs
+++ BlogCore/Controllers/BK_ArticlesController.cs
@@ -7,17 +7,32 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogCore.Controllers
-{
+{ 
+
+
     [Route("api/[controller]")]
     [ApiController]
     public class BK_ArticlesController : ControllerBase
     {
 
+        /// <summary>
+        /// 这是一种查询
+        /// </summary>
+        /// <returns></returns>
        [HttpGet]
         public BK_Articles query()
         {
             BK_Articles bK_Articles = new BK_Articles { User_id = 1, Article_comment_count = "0", Article_content = "zzzz" };
             return bK_Articles;
         }
+
+        [Route("Getby")]
+        [HttpGet]
+        public BK_Articles GetbyThis()
+        {
+            BK_Articles bK_Articles = new BK_Articles { User_id = 2, Article_comment_count = "111", Article_content = "时间" };  
+            return bK_Articles;
+        }
+         
     }
 }
